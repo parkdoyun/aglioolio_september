9f4d58176362af633d55fd794f64f09bbd1ed675
KevRiver <kanggakky1214@gmail.com>
2022-11-10 14:33:18 +0900

    세그먼트 트리 기본 문제

--- kev/221110/2042.cpp
+++ kev/221110/2042.cpp
@@ -1,127 +1,98 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-// 솔루션 참고
-
-#define rep(i, n) for(int i = 0; i < (n); i++)
-
-struct Command{
+struct Query{
     int a;
     int b;
     long long c;
 };
 
 struct Bound{
-    int start, end;
+    int start;
+    int end;
 };
 
-long long* segtree;
-long long* seq;
-Command* cmds;
+vector<long long> segtree;
+vector<long long> seq;
+vector<Query> queries;
 
-long long init(int i, Bound b){
-    if(b.start == b.end) return segtree[i] = seq[b.start];
-    int mid = (b.start + b.end) >> 1;
-    return segtree[i] = init(i * 2, {b.start, mid}) + init(i * 2 + 1, {mid + 1, b.end});
+void solve();
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+
+    return 0;
 }
 
-/**
- * @brief 특정 구간합을 구한다
- * 
- * @param i segtree idx
- * @param b segment bound 
- * @param t target segment bound
- * @return long long 
- */
-long long get_partial_sum(int i, Bound b, Bound t){
-    if(t.start > b.end || t.end < b.start)
+long long init(int root, Bound bound){
+    if(bound.start == bound.end)
+        return segtree[root] = seq[bound.start];
+    int mid = (bound.start + bound.end) / 2;
+    return segtree[root] = init(root*2, {bound.start, mid}) + init(root*2 + 1, {mid + 1, bound.end});
+}
+
+void update(int root, Bound bound, int t, long long diff){
+    if(t < bound.start || t > bound.end)
+        return;
+    
+    segtree[root] += diff;
+
+    if(bound.start == bound.end)
+        return;
+    
+    int mid = (bound.start + bound.end) / 2;
+    update(root*2, {bound.start, mid}, t, diff);
+    update(root*2 + 1, {mid + 1, bound.end}, t, diff);
+}
+
+long long get_partial_sum(int root, Bound u, Bound t){
+    if(t.start > u.end || t.end < u.start)
         return 0;
     
-    if(t.start <= b.start && b.end <= t.end)
-        return segtree[i];
+    if(t.start <= u.start && u.end <= t.end)
+        return segtree[root];
     
-    int mid = (b.start + b.end) >> 1;
-    int left_child = get_partial_sum(i*2, {b.start, mid}, t);
-    int right_child = get_partial_sum(i*2 + 1, {mid + 1, b.end}, t);
-    
-    return left_child + right_child;
-}
-
-/**
- * @brief segment tree 업데이트
- * 
- * @param i segtree index
- * @param b segment bounds
- * @param ti target index
- * @param d 변경된 타겟 인덱스 차이
- */
-void update(int i, Bound b, int ti, long long d){
-    if(ti < b.start || ti > b.end) return;
-    
-    segtree[i] += d;
-    
-    if(b.start == b.end) return;
-    
-    int mid = (b.start + b.end) >> 1;
-    
-    update(i*2, {b.start, mid}, ti, d);
-    update(i*2 + 1, {mid + 1, b.end}, ti, d);
+    int mid = (u.start + u.end) / 2;
+    return get_partial_sum(root*2, {u.start, mid}, t) + get_partial_sum(root*2 + 1, {mid + 1, u.end}, t);
 }
 
 void solve(){
     int N, M, K;
     cin >> N >> M >> K;
 
-    seq = new long long[N];
-    rep(i, N)
+    seq.resize(N);
+    for(int i = 0; i < N; i++){
         cin >> seq[i];
-    
-    cmds = new Command[M + K];
-    rep(i, M + K){
-        cin >> cmds[i].a
-            >> cmds[i].b
-            >> cmds[i].c;
     }
 
-    // initialize segtree
+    queries.resize(M + K);
+    for(int i = 0; i < M + K; i++){
+        cin >> queries[i].a
+            >> queries[i].b
+            >> queries[i].c;
+    }
+
     int h = (int)ceil(log2(N));
-    int size = 1 << (h + 1);
-    segtree = new long long[size];
+    int tree_size = 1 << (h + 1);
+    segtree.resize(tree_size);
     init(1, {0, N - 1});
 
-    const int UPDATE = 1;
-    const int PARTIAL_SUM = 2;
-    for(int i = 0; i < M + K; i++){
-        int opcode = cmds[i].a;
-        switch (opcode)
-        {
-        case UPDATE:
-            {
-                int t = cmds[i].b - 1;
-                long long diff = cmds[i].c - seq[t];
-                seq[t] = cmds[i].c;
-                update(1, {0, N - 1}, t, diff);
-            }
-            break;
-        case PARTIAL_SUM:
-            {
-                Bound u = {0, N - 1};
-                Bound t = {cmds[i].b - 1, (int)(cmds[i].c - 1)};
-                long long result = get_partial_sum(1, u, t);
-                cout << result << '\n';
-            }
-            break;
+    for(const auto& q: queries){
+        int a = q.a, b = q.b;
+        long long c = q.c;
+
+        if(a == 1){ // update
+            long long diff = c - seq[b - 1];
+            seq[b - 1] = c;
+            update(1, {0, N - 1}, b - 1, diff);
+        }
+        else{ // get partial sum
+            cout << get_partial_sum(1, {0, N-1}, {b-1, (int)c - 1}) << '\n';
         }
     }
-}
-
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-     solve();
-
-    return 0;
 }
