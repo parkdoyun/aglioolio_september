462b69d60e4477b8277c959f09ab7295a1e9beed
KevRiver <kanggakky1214@gmail.com>
2023-02-25 15:46:58 +0900

    백준 7662

---
+++ kev/230126/7662.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+
+vector<int> vec = {1, 2, 3, 4, 5};
+
+int main(){
+
+    for(const auto& e: vec)
+        cout << e << " ";
+    cout << "\n";
+
+    return 0;
+}
